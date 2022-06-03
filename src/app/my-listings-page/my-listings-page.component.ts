55956f54e6c635f983300601a397e4c344388a27
Abhijit Utkarsh <abhijitu18@gmail.com>
2022-06-03 19:35:19 +0530

    last-update

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyListingsPageComponent implements OnInit {
 
+  listings: Listing[] =[];
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeMyListings;
   }
-
+ 
+  onDeleteClicked(listingId: string):void{
+    alert(`Deleting your listing with ID ${listingId}`);
+  }
 }
